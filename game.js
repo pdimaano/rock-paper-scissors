2eeb4142cf9519c290fd302fe250d9e0d5c26ce8
Philip Dimaano <pdimaano7@gmail.com>
2021-12-01 19:46:55 -0800

    initial game

--- game.js
+++ game.js
@@ -1,29 +1,45 @@
+
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
 function playRound(playerSelection, computerSelection) {
+    let playerScore = 0
+    let computerScore = 0
     let result = ""
 
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
        (playerSelection == 'scissors' && computerSelection == 'paper') ||
        (playerSelection == 'paper' && computerSelection == 'rock')) {
            
-           result = ('You win! ' + playerSelection + ' beats ' + computerSelection)
+           playerScore += 1
+           result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+           + 'Player score: ' + playerScore + 'Computer score ' + computerScore)
+
+           if (playerScore == 5) {
+               result += 'You win! Reload the page to play again'
+           }
        }
     
     else if (playerSelection == computerSelection) {
-        result = ('A tie! You both chose ' + playerSelection)
+        result = ('A tie! You both chose ' + playerSelection
+        + 'Player score: ' + playerScore + 'Computer score ' + computerScore);
     }
 
     else {
-        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection)
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+        + 'Player score: ' + playerScore + 'Computer score ' + computerScore);
+        
+        if (computerScore == 5) {
+            result += 'You lost! Reload the page to play again'
+        }
     }
     
     return result
 }
 
-const playerSelection = 'paper';
-const computerSelection = computerPlay();
+computerSelection = computerPlay();
+playerSelection = window.prompt('Please input rock, paper or scissors: ');
 console.log(playRound(playerSelection, computerSelection));
