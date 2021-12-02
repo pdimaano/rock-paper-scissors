6545dd05772b0e6ff58682bda884ad978343f28b
Philip Dimaano <pdimaano7@gmail.com>
2021-12-01 18:57:48 -0800

    initial game

--- game.js
+++ game.js
@@ -3,7 +3,27 @@ function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-function playRound(playerSelection) {
-    let computerSelection = computerPlay()
+function playRound(playerSelection, computerSelection) {
     let result = ""
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+       (playerSelection == 'scissors' && computerSelection == 'paper') ||
+       (playerSelection == 'paper' && computerSelection == 'rock')) {
+           
+           result = ('You win! ' + playerSelection + ' beats ' + computerSelection)
+       }
+    
+    else if (playerSelection == computerSelection) {
+        result = ('A tie! You both chose ' + playerSelection)
+    }
+
+    else {
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection)
+    }
+    
+    return result
 }
+
+const playerSelection = 'paper';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
